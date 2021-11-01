47249e4b824edf6bac7c41db3ba547ed71330ae7
mskanaud <indeepbluenet@gmail.com>
2021-11-01 18:00:48 +0530

    some re adjustment

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,9 +5,15 @@ import FirstPage from './home/FirstPage';
 
 const Home = () => {
 	return(
-		<div className="home_container">
-			<MenuBar />
-			<FirstPage />
+		<div className="home-container">
+			<div className="full-height-width-flex">
+				<MenuBar />
+				<FirstPage />
+			</div>
+			<div className="full-height-width-flex">
+			</div>
+			<div className="full-height-width-flex">
+			</div>
 		</div>
 
 	);
--- src/containers/home/FirstPage.js
+++ src/containers/home/FirstPage.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, Image } from 'semantic-ui-react';
 const FirstPage = () => {
 	return (
 		<div className="description">
 			<div>
 				<h1 className="typewriter"> 
-					Hello, I'm <span className="base-color">Manjeet Singh.</span>
+					Hello, I'm <span className="base-color">Manjeet Singh. </span> 
+					Full Stack Web Developer
 				</h1>
-				<Header as ='h1'> 
-					I'm a Full Stack Web Developer.
-				</Header>
+			
+				<p className="p-text">
+					I design and code beautifully simple things, and I love what I do.
+				</p>
+				<div className="msk-avatar">
+					<Image src='/mf-avatar.svg' 
+					size='small' circular />
+				</div>
+			</div>
+			<div className="bottom-content">
+				<Image src='/hero.svg'/>
 			</div>
 		</div>
 	);
