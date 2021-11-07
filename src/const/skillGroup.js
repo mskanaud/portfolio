5dc48e878d2231870d766b0bdd4321ae1764aa33
mskanaud <indeepbluenet@gmail.com>
2021-11-07 06:12:12 +0530

    skiils progress

---
+++ src/const/skillGroup.js
@@ -0,0 +1,35 @@
+const skillGroup = [
+	{
+		name:'React Js (Semantic ui, Redux)',
+		ratio:7
+	},
+	{
+		name:'Java Script, HTML/CSS',
+		ratio:7
+	},
+	{
+		name:'Angular Js',
+		ratio:5
+	},
+	{
+		name:'Node Js(Express Js, Sequelize ORM)',
+		ratio:7
+	},
+	{
+		name:'React Native(Native Base)',
+		ratio:6
+	},
+	{
+		name:'PHP(Symphony Framework, Doctrine ORM)',
+		ratio:6
+	},
+	{
+		name:'Rest API, AWS, MySQL, GIT',
+		ratio:8
+	}, {
+		name:'Scrum and Agile Methodologies',
+		ratio:8
+	}	
+]
+
+export default skillGroup;
--- src/containers/home/SecondPage.js
+++ src/containers/home/SecondPage.js
@@ -1,12 +1,30 @@
 import React,{useState, useEffect} from 'react';
 import skills from '../../const/skills';
+import skillGroup from '../../const/skillGroup';
 import IconSkill from '../../components/IconSkill';
+import {Progress, Image, Label} from 'semantic-ui-react';
 
 const getSkillGroup = () => {
 	return skills.map(skill=> (
 		<IconSkill skill = {skill}>
 		</IconSkill>
 	));
+}
+const getSkillGroupProgress  = (offset, limit) => {
+	const skills = [];
+	for (let i=offset; i<limit;i++) {
+		const skill = skillGroup[i];
+		skills.push(
+			<>
+				<Label pointing ='below' basic>
+				 {skill.name}
+				</Label>
+				<Progress  size='small' 
+					success value={skill.ratio} total='10' progress='ratio' />
+			</>
+		);
+	}
+	return skills;
 }
 
 const SecondPage = () => {
@@ -23,6 +41,34 @@ const SecondPage = () => {
 				<div className="skill-group">
 					<div className="skill-container">
 						{skillGroup}
+					</div>
+				</div>
+			</div>
+			<div className="skill-progress">
+				<div className="skill-progress-content">
+					<div className="avatar-content">
+						<div className="skill-progress-avatar"> 
+							<div className="msk-avatar progress-avatar">
+								<Image src='/mf-avatar.svg' 
+								size='small' circular />
+							</div>
+						</div>
+						<div className="skill-progress-description">
+							<h2>Who's this guy?</h2>
+							<p>
+								I'm am Full Stack Developer.
+								I have serious passion for UI effects, and creating intuitive, dynamic user experiences.
+								Let's make something special.
+							</p>
+						</div>
+					</div>
+					<div className="skill-progress-loaders">
+						<div className="skill-progress-left">
+							{getSkillGroupProgress(0,4)}
+						</div>
+						<div className="skill-progress-right">
+							{getSkillGroupProgress(4,7)}
+						</div>
 					</div>
 				</div>
 			</div>
